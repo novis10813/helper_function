08d6127ec8806361b708acfaf19e609009cd6588
novis10813 <zhizhongz895253@gmail.com>
2021-10-09 14:53:38 +0800

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -273,3 +273,26 @@ def calculate_results(y_true, y_pred):
                   "recall": model_recall,
                   "f1": model_f1}
   return model_results
+
+# View an image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import random
+
+def view_random_image(target_dir, target_class):
+  # Setup target directory (we'll view images from here)
+  target_folder = target_dir+target_class
+
+  # Get a random image path
+  random_image = random.sample(os.listdir(target_folder), 1)
+
+  # Read in the image and plot it using matplotlib
+  img = mpimg.imread(target_folder + "/" + random_image[0])
+  plt.imshow(img)
+  plt.title(target_class)
+  plt.axis("off");
+
+  print(f"Image shape: {img.shape}") # show the shape of the image
+
+  return img
