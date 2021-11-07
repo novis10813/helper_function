574a75ed354724bea92cfa37bd15f7dea0339db5
novis10813 <zhizhongz895253@gmail.com>
2021-11-07 14:04:59 +0800

    Update tf_helpers.py

--- tf_helpers.py
+++ tf_helpers.py
@@ -22,33 +22,33 @@ def create_tensorboard_callback(dir_name, experiment_name):
 import matplotlib.pyplot as plt
 
 def plot_loss_curves(history):
-  """
-  Returns separate loss curves for training and validation metrics.
-  Args:
-    history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
-  """ 
-  loss = history.history['loss']
-  val_loss = history.history['val_loss']
+    """
+    Returns separate loss curves for training and validation metrics.
+    Args:
+        history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
+    """ 
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
 
-  accuracy = history.history['accuracy']
-  val_accuracy = history.history['val_accuracy']
+    accuracy = history.history['accuracy']
+    val_accuracy = history.history['val_accuracy']
 
-  epochs = range(len(history.history['loss']))
+    epochs = range(len(history.history['loss']))
 
-  # Plot loss
-  plt.plot(epochs, loss, label='training_loss')
-  plt.plot(epochs, val_loss, label='val_loss')
-  plt.title('Loss')
-  plt.xlabel('Epochs')
-  plt.legend()
+    # Plot loss
+    plt.plot(epochs, loss, label='training_loss')
+    plt.plot(epochs, val_loss, label='val_loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
 
-  # Plot accuracy
-  plt.figure()
-  plt.plot(epochs, accuracy, label='training_accuracy')
-  plt.plot(epochs, val_accuracy, label='val_accuracy')
-  plt.title('Accuracy')
-  plt.xlabel('Epochs')
-  plt.legend()
+    # Plot accuracy
+    plt.figure()
+    plt.plot(epochs, accuracy, label='training_accuracy')
+    plt.plot(epochs, val_accuracy, label='val_accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend()
 
 def compare_historys(original_history, new_history, initial_epochs=5):
     """
